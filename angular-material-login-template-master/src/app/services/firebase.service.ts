68d008a51ecca89ab8eb32cf7c7f208919b4fd27
ahmedomri <omri6346@gmail.com>
2021-01-03 23:48:28 +0100

    update

--- angular-material-login-template-master/src/app/services/firebase.service.ts
+++ angular-material-login-template-master/src/app/services/firebase.service.ts
@@ -8,14 +8,21 @@ import { Router } from '@angular/router';
 export class FirebaseService {
 
   isLoggedIn = false
+  errorLogin;
+  errorRegister;
+
   constructor(public firebaseAuth : AngularFireAuth,public router: Router) { }
+
+
   async signin(email: string, password : string){
     await this.firebaseAuth.signInWithEmailAndPassword(email,password)
     .then(res=>{
       this.isLoggedIn = true
       localStorage.setItem('user',JSON.stringify(res.user))
       this.router.navigate(['/home']);
-    })
+    }).catch((error) => {
+        this.errorLogin=error;
+      })
   }
   async signup(email: string, password : string){
     await this.firebaseAuth.createUserWithEmailAndPassword(email,password)
@@ -23,6 +30,8 @@ export class FirebaseService {
       this.isLoggedIn = true
       localStorage.setItem('user',JSON.stringify(res.user))
       this.router.navigate(['/home']);
+    }).catch((error) => {
+      this.errorRegister=error;
     })
   }
   logout(){
